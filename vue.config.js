db323eb2e1bea1c4c0d6540a2e1a026a618c6519
Maksim Demidov <maaksaa@gmail.com>
2022-03-27 11:47:32 +0300

    Настройка деплоя на гитхаб

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+
+// файл vue.config.js должен быть расположен в корневом каталоге проекта
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/need-for-drive/'
+    : '/'
+}
